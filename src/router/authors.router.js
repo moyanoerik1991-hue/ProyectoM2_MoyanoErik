00e5e4ec3f417c40b5a0846c0fbb1ca251c12250
moyanoerikdemian <moyanoerik1991@gmail.com>
2026-08-10 18:52:00 -0300

    feat: add authors routes

---
+++ src/router/authors.router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const authorsController = require('../controllers/authors.controller');
+
+const router = express.Router();
+
+router.get('/', authorsController.getAuthors);
+router.get('/:id', authorsController.getAuthorById);
+router.post('/', authorsController.createAuthor);
+router.put('/:id', authorsController.updateAuthor);
+router.delete('/:id', authorsController.deleteAuthor);
+
+module.exports = router;
