975ea136225f516268637b8bb498fa9344ec68d7
moyanoerikdemian <moyanoerik1991@gmail.com>
2026-08-11 06:57:12 -0300

    feat: add comments router to server configuration

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { router } = require('./app');
 const authorsRouter = require('./router/authors.router');
+const commentsRouter = require('./router/comments.router');
 const postsRouter = require('./router/posts.router');
 const errorHandler = require('./middleware/errorHandler');
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(router);
 app.use('/authors', authorsRouter);
+app.use('/comments', commentsRouter);
 app.use('/posts', postsRouter);
 app.use(errorHandler);
 
