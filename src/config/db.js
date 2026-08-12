729ad1e6fb170023ed6254d244dc276dc679a403
moyanoerikdemian <moyanoerik1991@gmail.com>
2026-08-11 23:57:36 -0300

    feat: update database configuration variables in db.js

--- src/config/db.js
+++ src/config/db.js
@@ -7,9 +7,13 @@ dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
+  max: Number(process.env.DB_MAX),
+  idleTimeoutMillis: Number(process.env.DB_IDLETIMEOUTMILLIS),
+  connectionTimeoutMillis: Number(process.env.DB_CONNECTIONTIMEOUTMILLIS),
+  connectionString: process.env.DATABASE_URL
 });
 
 module.exports = pool;
