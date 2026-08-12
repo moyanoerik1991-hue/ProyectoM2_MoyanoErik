9b2bb1d81ff03bee0d77d22cc9b7879fbaf949e5
moyanoerikdemian <moyanoerik1991@gmail.com>
2026-08-11 23:57:21 -0300

    feat: add OpenAPI documentation endpoint and Swagger UI integration

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const { router } = require('./app');
 const authorsRouter = require('./router/authors.router');
 const commentsRouter = require('./router/comments.router');
@@ -6,9 +9,21 @@ const postsRouter = require('./router/posts.router');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
+const swaggerDocument = YAML.load(path.join(__dirname, '../docs/openapi.yaml'));
 
 app.use(express.json());
 app.use(router);
+
+app.get('/api/openapi.json', (req, res) => {
+  res.json(swaggerDocument);
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use('/api/authors', authorsRouter);
+app.use('/api/comments', commentsRouter);
+app.use('/api/posts', postsRouter);
+
 app.use('/authors', authorsRouter);
 app.use('/comments', commentsRouter);
 app.use('/posts', postsRouter);
